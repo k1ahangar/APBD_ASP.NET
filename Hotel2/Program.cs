8c66275f7d8521ff270ac16311d0ea89516cf122
Keyvan Ahangar <keyvan.ahangar2017@gmail.com>
2026-04-20 22:39:38 +0200

    Add files via upload

---
+++ Hotel2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Program.cs
+using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.MapControllers();
+
+app.Run();
