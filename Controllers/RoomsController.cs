69063999013eaa9030d1929b879920a3a0d53599
Keyvan Ahangar <keyvan.ahangar2017@gmail.com>
2026-04-20 22:43:13 +0200

    Add files via upload

---
+++ Controllers/RoomsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
+
+namespace Hotel2.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RoomsController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Rooms working!");
+    }
+}
